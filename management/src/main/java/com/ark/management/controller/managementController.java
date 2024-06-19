61143ca7c64dad78c70dd7f59a011ac74a6f2ead
AkhilReddy16 <kommareddyakhilreddy@gmail.com>
2024-06-19 11:57:36 -0400

    added student model

--- management/src/main/java/com/ark/management/controller/managementController.java
+++ management/src/main/java/com/ark/management/controller/managementController.java
@@ -1,5 +1,16 @@
 package com.ark.management.controller;
 
+import com.ark.management.model.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/management")
 public class managementController {
-    public void addStudent(){}
+
+    @PostMapping("/addStudent")
+    public ResponseEntity<Student> addStudent(@RequestBody Student student){
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
 }
---
+++ management/src/main/java/com/ark/management/model/Student.java
@@ -0,0 +1,25 @@
+package com.ark.management.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+public class Student implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private long admissionYear;
+    private String course;
+}
